21c4f9a3c1c28790678becb11fe7edffeb03aa2d
Ilyasa Azmi <theazmi99ilyasa@gmail.com>
2019-07-09 15:02:40 +0700

    Initial Commit

---
+++ AutoLayout2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout2
+//
+//  Created by Ilyasa Azmi on 09/07/19.
+//  Copyright © 2019 Ilyasa Azmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
